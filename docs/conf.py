67f172653bd5c5a1424b90217bd5f112a451c9ab
Parici75 <benjamin.roland@hotmail.fr>
2026-05-13 00:50:16 +0200

    fix: disable size data range normalization below threshold
    * fix scatter3d hover info attribute
    * improve scatter3d marker aesthetics
    * fix sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -70,6 +70,8 @@ autodoc_pydantic_model_show_json = False
 autodoc_pydantic_model_show_config_summary = False
 autodoc_pydantic_model_show_field_summary = True
 
+# Autosectionlabel settings
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
